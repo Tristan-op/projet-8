25079fc8b39d1bb1737c8c63936c9d8158cc7cac
Tristan-op <tristan.brioulet@gmail.com>
2024-12-27 09:18:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-# Initialisation de l'application FastAPI
 app = FastAPI()
 
-# =========================================
-# ROUTES
-# =========================================
-
-@app.get("/", response_class=HTMLResponse)
-async def main_page():
-    return """
-    <html>
-    <head>
-        <title>Accueil</title>
-    </head>
-    <body>
-        <h1>Hello World</h1>
-    </body>
-    </html>
-    """
-
-# =========================================
-# Lancer l'application (si nécessaire localement)
-# =========================================
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
